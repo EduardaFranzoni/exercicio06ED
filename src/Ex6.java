616b0d3b8f1667d5a0d4c73841d300c23b9a2b69
Franz <Franz@LAPTOP-H968BU93>
2020-10-15 02:15:39 -0300

    Exercício06

--- src/Ex6.java
+++ src/Ex6.java
@@ -1,4 +1,27 @@
 
 public class Ex6 {
 
+    public static void main(String[] args) {
+        int[] c = { 13, 4, 10, 23, 6 };
+        System.out.println(maior(c, 0, 4));
+    }
+
+    public static int maior(int v[], int inicio, int fim) {
+        int meio = (inicio + fim) / 2;
+        int n1, n2;
+        if (meio > inicio) {
+            n1 = maior(v, inicio, meio);
+            n2 = maior(v, meio + 1, fim);
+        } else {
+            n1 = v[inicio];
+            n2 = v[fim];
+        }
+        if (n1 < n2)
+            return n1;
+        else
+            return n2;
+    }
 }
+
+
+
